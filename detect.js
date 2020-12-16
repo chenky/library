aae04acf5a6682d6d6c43f89981c40529a15b7d2
chenqiliang <chenqiliang@hdsc.com>
2020-12-16 17:19:07 +0800

    客户端检测detect.js

--- detect.js
+++ detect.js
@@ -64,9 +64,12 @@ function detect (ua, platform) {
   return { os, browser }
 }
 
+
+// 如下是简单的一下判断，如果需要更全面的判断可以使用如下两个类库
+// https://github.com/lancedikson/bowser
+// https://github.com/etienne-martin/device-detector-js
 const ua = navigator.userAgent
 const platform = navigator.platform
-
 
 // 判断是iphone
 const isiPhone = /(iPhone\sOS)\s([\d_]+)/.test(ua)
@@ -78,29 +81,52 @@ const isiPod = /(iPod)(.*OS\s([\d_]+))?/.test(ua)
 const isIos = isiPhone || isiPad || isiPod
 // 判断是否mac os即 osx
 const isOsx = /Macintosh/.test(ua)
-
 // 判断是android平台
 const isAndroid = /(Android);?[\s\/]+([\d.]+)?/.test(ua)
-
 // 判断是否windows平台
 const isWin = /Win\d{2}|Windows/.test(platform)
-
+/**
+ * windows2000: Windows NT 5.0|Windows 2000
+ * windowsXp: Windows NT 5.1|Windows XP
+ * windows2003:Windows NT 5.2|Windows 2003
+ * windowsVista: Windows NT 6.0|Windows Vista
+ * windows7: Windows NT 6.1|Windows 7
+ * */
 // 判断是否windows phone平台
 const isWp = /Windows Phone ([\d.]+)/.test(ua)
+// 判断是否linux
+const isLinux = /Linux/i.test(platform)
+// 华为
+const isHuawei = /huawei/i.test(ua)
+// Nexus
+const isNexus = /nexus\s*(?:\d).*|galaxy nexus/i.test(ua)
 
+// mobile
+const isMobile = /[^-]mobi/i.test(ua)
 // 判断是否微信
 const isWx = /micromessenger/i.test(ua)
+// 判断是否支付宝
+const isAlipay = /Alipay/i.test(ua)
 // 判断是否qq客户端
 const isQQ = /QQ\/[0-9]/i.test(ua)
-
-//is ie 
+// 判断是否qq browser
+const isQQBrowser = /qqbrowser/i.test(ua)
+// 判断是否uc browser
+const isUcBrowser = /ucbrowser/i.test(ua)
+// is opera
+const isOpera = /opera|opr\/|opios/i.test(ua)
+// is Samsung Browser
+const isSamsung = /SamsungBrowser/i.test(ua)
+// is ie 
 const isIe = /MSIE|Trident/.test(ua)
+// is edge
+const isEdge = /\sedg\/|edg([ea]|ios)/i.test(ua)
 //is firefox 
-const isFirefox = /Firefox\/([\d.]+)/.test(ua)
+const isFirefox = /(?:firefox|iceweasel|fxios)[\s/](\d+(\.?_?\d+)+)/i.test(ua)
 // is firefoxos 
 const isFirefoxOs = /\((?:Mobile|Tablet); rv:([\d.]+)\).*Firefox\/[\d.]+/.test(ua)
 // is chrome 
-const isChrome = /Chrome|CriOS/.test(ua)
+const isChrome = /(?:chrome|crios|crmo)\/(\d+(\.?_?\d+)+)/i.test(ua)
 // is safari
-const isSafari = /Safari/i.test(ua)
+const isSafari = /(iPhone|iPod|iPad).*AppleWebKit(?=.*Safari)/.test(ua)
 
