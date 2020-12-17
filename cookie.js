a0fa56743b331113544ee475014647385492844a
chenqiliang <chenqiliang@hdsc.com>
2020-12-17 08:57:24 +0800

    优化cookie设置和获取的方法

--- cookie.js
+++ cookie.js
@@ -1,24 +1,18 @@
 // 获取cookie、
-export function getCookie(name) {
-  var arr = null
-  var reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
-  // eslint-disable-next-line no-cond-assign
-  if (arr = document.cookie.match(reg)) {
-    return (arr[2])
-  } else {
-    return null
-  }
+function getCookie (name) {
+  var reg = new RegExp("(^| )" + name + "(?:=([^;]*))?(;|$)"), val = document.cookie.match(reg)
+  return val ? (val[2] ? decodeURIComponent(val[2]) : "") : null
 }
 
 // 设置cookie,增加到vue实例方便全局调用
-export function setCookie(cname, value, expiredays) {
-  var exdate = new Date()
-  exdate.setDate(exdate.getDate() + expiredays)
-  document.cookie = cname + '=' + escape(value) + ((expiredays == null) ? '' : ';expires=' + exdate.toGMTString())
+function setCookie (name, value, expires, path, domain, secure) {
+  var exp = new Date(), expires = arguments[2] || null, path = arguments[3] || "/", domain = arguments[4] || null, secure = arguments[5] || false
+  expires ? exp.setMinutes(exp.getMinutes() + parseInt(expires)) : ""
+  document.cookie = name + '=' + encodeURIComponent(value) + (expires ? ';expires=' + exp.toGMTString() : '') + (path ? ';path=' + path : '') + (domain ? ';domain=' + domain : '') + (secure ? ';secure' : '')
 }
 
 // 删除cookie
-export function delCookie(name) {
+export function delCookie (name) {
   var exp = new Date()
   exp.setTime(exp.getTime() - 1)
   var cval = getCookie(name)
