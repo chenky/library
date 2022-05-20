e6ae0510488957b6a16f93fc280c216598652300
qiliang <chenqiliang@surveyplus.cn>
2022-05-20 17:26:16 +0800

    正整数正则表达式

--- reg.js
+++ reg.js
@@ -8,144 +8,144 @@
 */
 //第二代身份证号码正则
 let isChineseID2 =
-  /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
+    /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/
 //第一代身份证正则表达式(15位)
-let isChineseID1 = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/;
+let isChineseID1 = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/
 // 身份证正则表达式包括第一代和第二代
 let idCode =
-  /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
+    /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/
 ////用户名正则，4到16位（字母，数字，下划线，减号）
-let isUserName = /^[a-zA-Z0-9_-]{4,16}$/;
+let isUserName = /^[a-zA-Z0-9_-]{4,16}$/
 //密码正则，以字母开头，长度在6~18之间，只能包含字母、数字和下划线
-let isPassword = /^[a-zA-Z]\w{5,17}$/;
+let isPassword = /^[a-zA-Z]\w{5,17}$/
 //强密码正则，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
 let isStrongPassword =
-  /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/;
+    /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/
 //非6位数字
-let isPwd = /^[^\d]{6}$/;
+let isPwd = /^[^\d]{6}$/
 //连续的3位数字  123587
-let continuousReg = /^\d*(012|123|234|345|456|567|678|789)\d*$/;
+let continuousReg = /^\d*(012|123|234|345|456|567|678|789)\d*$/
 //重复的3位数 如 123123
-let repeatReg = /^(\d{3})\1$/;
+let repeatReg = /^(\d{3})\1$/
 // 判断是否有连续重复的字符
-let repeatCharacter = /([a-zA-Z])\1/;
+let repeatCharacter = /([a-zA-Z])\1/
 //倒序的3位数 如 123321
-let ReverseOrder = /^(\d)(\d)(\d)\3\2\1$/;
+let ReverseOrder = /^(\d)(\d)(\d)\3\2\1$/
 //最多只包含两个不同的数字
-let maxTwo = /^(\d)\1*([^\1])(\1|\2)*$/;
+let maxTwo = /^(\d)\1*([^\1])(\1|\2)*$/
 // qq号正则
-let isQQ = /^[1-9][0-9]{4,10}$/;
+let isQQ = /^[1-9][0-9]{4,10}$/
 //微信号正则，6至20位，以字母开头，字母，数字，减号，下划线
-let isWechat = /^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$/;
+let isWechat = /^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$/
 //特殊字符检测正则:
-let isSpecialCharacters = /["'<>%;)(&+]+-!！@#$~/;
+let isSpecialCharacters = /["'<>%;)(&+]+-!！@#$~/
 // 域名正则
 let isDomain =
-  /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/;
+    /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/
 // 车牌号码正则
 let isLicensePlateNumber =
-  /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/;
+    /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/
 // 包含中文正则
-let isChinese = /[\u4E00-\u9FA5]/;
+let isChinese = /[\u4E00-\u9FA5]/
 // 匹配所有单字节长度的字符组成的字符串
-let isSingleByte = /^[\x00-\xff]+$/;
+let isSingleByte = /^[\x00-\xff]+$/
 // 匹配双字节字符(包括汉字在内) ：[^\x00 -\xff]
-let isDoubleByte = /[^\x00-\xff]/;
+let isDoubleByte = /[^\x00-\xff]/
 // 护照正则
 let isPassport =
-  /^(P\d{7}|G\d{7,8}|TH\d{7,8}|S\d{7,8}|A\d{7,8}|L\d{7,8}|\d{9}|D\d+|1[4,5]\d{7})$/;
+    /^(P\d{7}|G\d{7,8}|TH\d{7,8}|S\d{7,8}|A\d{7,8}|L\d{7,8}|\d{9}|D\d+|1[4,5]\d{7})$/
 // IP地址正则
-let isIP = /\d+\.\d+\.\d+\.\d+/;
+let isIP = /\d+\.\d+\.\d+\.\d+/
 //经度正则
 let isLongitude =
-  /^(\-|\+)?(((\d|[1-9]\d|1[0-7]\d|0{1,3})\.\d{0,6})|(\d|[1-9]\d|1[0-7]\d|0{1,3})|180\.0{0,6}|180)$/;
+    /^(\-|\+)?(((\d|[1-9]\d|1[0-7]\d|0{1,3})\.\d{0,6})|(\d|[1-9]\d|1[0-7]\d|0{1,3})|180\.0{0,6}|180)$/
 //纬度正则
-let isLatitude = /^(\-|\+)?([0-8]?\d{1}\.\d{0,6}|90\.0{0,6}|[0-8]?\d{1}|90)$/;
+let isLatitude = /^(\-|\+)?([0-8]?\d{1}\.\d{0,6}|90\.0{0,6}|[0-8]?\d{1}|90)$/
 // 某个固定域名
-let someDomain = /^https?:\/\/.+\.qtrade\.com\.cn\/.+$/;
+let someDomain = /^https?:\/\/.+\.qtrade\.com\.cn\/.+$/
 
 // http://jqueryvalidation.org/ 常用的正则验证
 // contributed by Scott Gonzalez = http://projects.scottsplayground.com/email_address_validation/
 let email =
     "/^((([a-z]|d|[!#$%&'*+-/=?^_`{|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(.([a-z]|d|[!#$%&'*+-/=?^_`{|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).?$/i",
-  // contributed by Scott Gonzalez = http://projects.scottsplayground.com/iri/
-  url =
-    "/^(https?|ftp)://(((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:)*@)?(((d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]))|((([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).?)(:d*)?)(/((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)+(/(([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)*)*)?)?(?((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)|[\uE000-\uF8FF]|/|?)*)?(#((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)|/|?)*)?$/i",
-  // http://docs.jquery.com/Plugins/Validation/Methods/number
-  // 科学计数格式化数字，可以带逗号
-  number = "/^-?(?:d+|d{1,3}(?:,d{3})+)(?:.d+)?$/",
-  // 保留两位小数点的数字，
-  number2 = "/^(0?|([1-9]d*))(?:.d{1,2})?$/",
-  // 正整数，小数合法性验证，其实就是钱验证，money验证,dollar验证，小数点后面保留5位，比上面的正则更严格精确
-  moneyReg = /^(0|([1-9]\d*))(?:\.\d{0,4}[1-9])?$/,
-  // http://docs.jquery.com/Plugins/Validation/Methods/digits
-  digits = "/^\d*\.?\d*$/",
-  isInt = "/^[1-9]\d*$/", // 正整数
-  // 验证中国邮政编码[1-9]{1}(\d+){5};
-  postalCode = "/^\d{6}$/",
-  // 验证中国电话
-  phone = "/^((\d{3,4})-{0,1}){0,1}(d{7,8})$/",
-  // 验证中国手机号
-  mobilePhone = "/^((13|15|18|14)\d{9})$/";
+    // contributed by Scott Gonzalez = http://projects.scottsplayground.com/iri/
+    url =
+        "/^(https?|ftp)://(((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:)*@)?(((d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]).(d|[1-9]d|1dd|2[0-4]d|25[0-5]))|((([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))).?)(:d*)?)(/((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)+(/(([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)*)*)?)?(?((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)|[\uE000-\uF8FF]|/|?)*)?(#((([a-z]|d|-|.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[da-f]{2})|[!$&'()*+,;=]|:|@)|/|?)*)?$/i",
+    // http://docs.jquery.com/Plugins/Validation/Methods/number
+    // 科学计数格式化数字，可以带逗号
+    number = "/^-?(?:d+|d{1,3}(?:,d{3})+)(?:.d+)?$/",
+    // 保留两位小数点的数字，
+    number2 = "/^(0?|([1-9]d*))(?:.d{1,2})?$/",
+    // 正整数，小数合法性验证，其实就是钱验证，money验证,dollar验证，小数点后面保留5位，比上面的正则更严格精确
+    moneyReg = /^(0|([1-9]\d*))(?:\.\d{0,4}[1-9])?$/,
+    // http://docs.jquery.com/Plugins/Validation/Methods/digits
+    digits = "/^\d*\.?\d*$/",
+    isInt = "/^[0-9]*\.?\d*$/", // 正整数
+    // 验证中国邮政编码[1-9]{1}(\d+){5};
+    postalCode = "/^\d{6}$/",
+    // 验证中国电话
+    phone = "/^((\d{3,4})-{0,1}){0,1}(d{7,8})$/",
+    // 验证中国手机号
+    mobilePhone = "/^((13|15|18|14)\d{9})$/"
 
 // qq域名正则表达式, 正则匹配qq子域名
 // 写一个正则表达式，能够识别qq.com的所有子域名
-const qqDomain = /^(\w+\.)*qq\.com$/;
+const qqDomain = /^(\w+\.)*qq\.com$/
 
 // reference: https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
 // 严格要求输入框是数值类型，也可以验证小数，其他正则验证， input number
 // Restricts input for the given textbox to the given inputFilter function.
 function setInputFilter(textbox, inputFilter) {
-  [
-    "input",
-    "keydown",
-    "keyup",
-    "mousedown",
-    "mouseup",
-    "select",
-    "contextmenu",
-    "drop",
-  ].forEach(function (event) {
-    textbox.addEventListener(event, function () {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      } else {
-        this.value = "";
-      }
-    });
-  });
+    [
+        "input",
+        "keydown",
+        "keyup",
+        "mousedown",
+        "mouseup",
+        "select",
+        "contextmenu",
+        "drop",
+    ].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value
+                this.oldSelectionStart = this.selectionStart
+                this.oldSelectionEnd = this.selectionEnd
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd)
+            } else {
+                this.value = ""
+            }
+        })
+    })
 }
 
 setInputFilter(document.getElementById("myTextBox"), function (value) {
-  return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
-});
+    return /^\d*\.?\d*$/.test(value) // Allow digits and '.' only, using a RegExp
+})
 // Install input filters.
 setInputFilter(document.getElementById("intTextBox"), function (value) {
-  return /^-?\d*$/.test(value);
-});
+    return /^-?\d*$/.test(value)
+})
 setInputFilter(document.getElementById("uintTextBox"), function (value) {
-  return /^\d*$/.test(value);
-});
+    return /^\d*$/.test(value)
+})
 setInputFilter(document.getElementById("intLimitTextBox"), function (value) {
-  return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 500);
-});
+    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 500)
+})
 setInputFilter(document.getElementById("floatTextBox"), function (value) {
-  return /^-?\d*[.,]?\d*$/.test(value);
-});
+    return /^-?\d*[.,]?\d*$/.test(value)
+})
 setInputFilter(document.getElementById("currencyTextBox"), function (value) {
-  return /^-?\d*[.,]?\d{0,2}$/.test(value);
-});
+    return /^-?\d*[.,]?\d{0,2}$/.test(value)
+})
 setInputFilter(document.getElementById("latinTextBox"), function (value) {
-  return /^[a-z]*$/i.test(value);
-});
+    return /^[a-z]*$/i.test(value)
+})
 setInputFilter(document.getElementById("hexTextBox"), function (value) {
-  return /^[0-9a-f]*$/i.test(value);
-});
+    return /^[0-9a-f]*$/i.test(value)
+})
 
 /*
   动态正则表达式，参数正则表达式
