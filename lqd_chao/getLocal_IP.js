a2ed126187bd075124231ce54b9afd344a78a422
chenky <cqiliang@163.com>
2023-11-20 22:41:00 +0800

    fix typescript check tip

--- lqd_chao/getLocal_IP.js
+++ lqd_chao/getLocal_IP.js
@@ -1,19 +1,20 @@
 import os from 'os'
 
-function getIPAddress () {
-    const networkInterfaces = os.networkInterfaces()
-
-    for (const interfaceName in networkInterfaces) {
-        const interfaces = networkInterfaces[interfaceName]
-
-        if (interfaces) {
-            for (const iface of interfaces) {
-                if (iface.family === 'IPv4' && !iface.internal) {
-                    return iface.address
-                }
-            }
-        }
-    }
-
-    return '0.0.0.0' // 默认使用 localhost
-}
+function getIPAddress() {
+    const networkInterfaces = os.networkInterfaces();
+    let result = '0.0.0.0'; // 默认使用 localhost
+  
+    Object.keys(networkInterfaces).forEach((interfaceName) => {
+      const interfaces = networkInterfaces[interfaceName];
+  
+      if (interfaces) {
+        interfaces.forEach((iface) => {
+          if (iface.family === 'IPv4' && !iface.internal) {
+            result = iface.address;
+          }
+        });
+      }
+    });
+  
+    return result;
+  }
